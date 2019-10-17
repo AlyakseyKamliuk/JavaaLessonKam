d398d6395e42f3278672d00367c782a522889ead
AlyakseyKamliuk <56596955+AlyakseyKamliuk@users.noreply.github.com>
2019-10-17 18:36:53 +0300

    Modified task 1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("     Задача 1");
-        intToString(18);
+        intToString(500000000);
         System.out.println("     Задача 2");
-        stringToInt("152");
+        stringToInt("1231233456");
         System.out.println("     Задача 3");
         evenNumberTest(12);
         evenNumberTest(11);
@@ -19,17 +22,20 @@ public class Main {
         System.out.println(containsChar("sdfgjfgnjn", 'a'));
         System.out.println(containsChar("sdfgjfgnjn", 'd'));
         System.out.println("     Задача 6");
-        timeConvert(20);
+        timeConvert((long) 50000000);
         System.out.println("     Задача 7");
-        timeConvert2((long) 200000);
+        timeConvert2((long) 50000000);
         System.out.println("     Задача 8");
         System.out.println(stringtoUpLowCase("SdfsaddDD"));
         System.out.println("     Задача 9");
         calcOfAreaAndCircum(5);
         System.out.println("     Задача 10");
-        System.out.println(ovRectAroun(12, 16, 6));
-        System.out.println("     Задача 11");
-        ageCalculate(1984, 6, 30);
+        System.out.println(isCircleCoversRectangle(10, -16, -6));
+        System.out.println("     Задача 11 (три способа)");
+        ageCalculate1(1984, 6, 30);
+        System.out.println(ageCalculate2(30,6,1984));
+        ageCalculate3(7,7,1972);
+
     }
 
     public static void intToString(int a) {
@@ -49,20 +55,21 @@ public class Main {
     }
 
     public static void stringToInt(String s) {
-        int a;
-        a = Integer.valueOf(s);
-        System.out.println("Способ 1:" + a);
+        long rez;
+        rez = Integer.valueOf(s);
+        System.out.println("Способ 1:" + rez);
 
-        a = Integer.parseInt(s);
-        System.out.println("Способ 2:" + a);
+        rez = Integer.parseInt(s);
+        System.out.println("Способ 2:" + rez);
 
-        char[] chars;
+      char[] chars;
         chars = s.toCharArray();
-        a = 0;
+
+        rez = 0;
         for (int i = 0; i < chars.length; i++) {
-            a = a + chars[i];
+            rez = rez*10 + Integer.parseInt((""+chars[i]));
         }
-        System.out.println("Способ 3:" + a);
+        System.out.println("Способ 3:" + rez);
 
     }
 
@@ -82,18 +89,18 @@ public class Main {
         return s.contains(String.valueOf(c));
     }
 
-    public static void timeConvert(int hours) {
+    public static void timeConvert(long hours) {
         System.out.println(hours + " час(ов) это " + hours * 60 + " минут, или " + hours * 3600 + " секунд");
     }
 
 
     public static void timeConvert2(Long seconds) {
-        Long day=seconds/86400;
-        Long ostatok=seconds%86400;
-        Long hours=ostatok/3600;
-        Long minutes=ostatok%3600/60;
-        Long secondsRez=ostatok%3600%60%60;
-        System.out.println(seconds+" секунд это:"+day+" дней "+hours+" часов "+minutes+" минут "+secondsRez +" секунд");
+        Long day = TimeUnit.SECONDS.toDays(seconds);
+        Long remainderOfDivision = seconds % 86400;
+        Long hours = remainderOfDivision / 3600;
+        Long minutes = remainderOfDivision % 3600 / 60;
+        Long secondsRez = remainderOfDivision % 3600 % 60 % 60;
+        System.out.println(seconds + " секунд это:" + day + " дней " + hours + " часов " + minutes + " минут " + secondsRez + " секунд");
 
     }
 
@@ -117,19 +124,31 @@ public class Main {
         System.out.println("Длина окружности " + 2 * Math.PI * radius);
     }
 
-    public static boolean ovRectAroun(int length, int width, int radius) {
+    public static boolean isCircleCoversRectangle(int length, int width, int radius) {
         double diagonal = Math.sqrt(length * length + width * width);
         return diagonal <= radius * 2;
     }
 
-    public static void ageCalculate(int year, int month, int day) {
-
+    public static void ageCalculate1(int year, int month, int day) {
         Calendar age = new GregorianCalendar();
-        age.add(Calendar.MONTH, -month - 1);
-        age.add(Calendar.YEAR, -year);
         age.add(Calendar.DAY_OF_MONTH, -day);
-        System.out.println(age.getTime());
-        System.out.println("Ваш возраст:"+age.get(1)+" лет");
+        age.add(Calendar.MONTH, -(month));
+        age.add(Calendar.YEAR, -year);
+        String buf = age.toString();
+        String[] chars = buf.split(",");
+        System.out.println("Ваш возраст составляет:" + chars[13].substring(chars[13].lastIndexOf('=') + 1) + " лет " + (Integer.parseInt(chars[14].substring(chars[14].lastIndexOf('=') + 1)) + 1) + " месяцев " + chars[17].substring(chars[17].lastIndexOf('=')+1) + " дней");
     }
 
+        public static String ageCalculate2(int day, int month, int year){
+
+            LocalDate birthday = LocalDate.of(year, month, day);
+            Period vozrast = Period.between(birthday, LocalDate.now());
+            return "Ваш возраст составляет:" + vozrast.getYears() + " лет " + vozrast.getMonths() + " месяцев " + vozrast.getDays() + " дней";
+
+    }
+
+    public static void ageCalculate3(int day, int month, int year) {
+        LocalDate rez = LocalDate.now().minus(Period.of(year, month, day));
+        System.out.println("Ваш возраст составляет:" + rez.getYear() + " лет " + rez.getMonthValue() + " месяцев " + rez.getDayOfMonth() + " дней");
+    }
 }
